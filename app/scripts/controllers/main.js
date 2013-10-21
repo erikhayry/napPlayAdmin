1f151c794fff93990c0183412f78432371ce8f7e
Erik Portin <erikportin@Eriks-MacBook-Pro-2.local>
2013-10-21 17:10:46 +0100

    Initial angualr setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('napPlayAdminApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
